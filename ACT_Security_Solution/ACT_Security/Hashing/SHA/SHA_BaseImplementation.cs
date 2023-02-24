a3e436d7b5fe6edd47bf4ea561726a0041d675c3
Mark Alicz <malicz@themxgroup.com>
2023-02-24 03:06:58 -0600

    Many Changes .Net 7

--- ACT_Security_Solution/ACT_Security/Hashing/SHA/SHA_BaseImplementation.cs
+++ ACT_Security_Solution/ACT_Security/Hashing/SHA/SHA_BaseImplementation.cs
@@ -3,85 +3,57 @@ using System.Security.Cryptography;
 
 namespace ACT.Core.Security.Hashing
 {
-    /// <summary>
-    /// SHA Hashing Class
-    /// </summary>
-    public static class SHAHashing
-    {
+   /// <summary>
+   /// SHA Hashing Class
+   /// </summary>
+   public static class SHAHashing
+   {
 
-        /// <summary>
-        /// Convert a input string to a byte array and compute the hash.
-        /// </summary>
-        /// <param name="value">Input string.</param>
-        /// <returns>The Hexadecimal string.</returns>
-        public static string StringToSHA256Hash(string value, bool removeDashes = true)
-        {
-            if (value.NullOrEmpty())
-            {
-                return value.ToString(true);
-            }
+      /// <summary>
+      /// Convert a input string to a byte array and compute the hash.
+      /// </summary>
+      /// <param name="value">Input string.</param>
+      /// <returns>The Hexadecimal string.</returns>
+      public static string StringToSHA256Hash(string value, bool removeDashes = true)
+      {
+         return value.StringToSHA256Hash(removeDashes);
+      }
 
-            using (var hasher = SHA256.Create())
-            {
-                var originalBytes = System.Text.Encoding.UTF8.GetBytes(value);
-                var encodedBytes = hasher.ComputeHash(originalBytes);
-                string _tmpReturn = "";
-                if (removeDashes) { _tmpReturn = BitConverter.ToString(encodedBytes).Replace("-", string.Empty).ToUpper(); }
-                else { _tmpReturn = BitConverter.ToString(encodedBytes); }
+      /// <summary>
+      /// Convert a input string to a byte array and compute the hash.
+      /// </summary>
+      /// <param name="value">Input string.</param>
+      /// <returns>The Hexadecimal string.</returns>
+      public static string ToSHA512Hash(string value, bool removeDashes = true)
+      {
+         if (value.NullOrEmpty())
+         {
+            return value.ToString(true);
+         }
 
-                return _tmpReturn;
-            }
-        }
+         using (var hasher = SHA512.Create())
+         {
+            var originalBytes = System.Text.Encoding.UTF8.GetBytes(value);
+            var encodedBytes = hasher.ComputeHash(originalBytes);
+            string _tmpReturn = "";
+            if (removeDashes) { _tmpReturn = BitConverter.ToString(encodedBytes).Replace("-", string.Empty).ToUpper(); }
+            else { _tmpReturn = BitConverter.ToString(encodedBytes); }
 
-        /// <summary>
-        /// Convert a input string to a byte array and compute the hash.
-        /// </summary>
-        /// <param name="value">Input string.</param>
-        /// <returns>The Hexadecimal string.</returns>
-        public static string ToSHA512Hash(string value, bool removeDashes = true)
-        {
-            if (value.NullOrEmpty())
-            {
-                return value.ToString(true);
-            }
-
-            using (var hasher = SHA512.Create())
-            {
-                var originalBytes = System.Text.Encoding.UTF8.GetBytes(value);
-                var encodedBytes = hasher.ComputeHash(originalBytes);
-                string _tmpReturn = "";
-                if (removeDashes) { _tmpReturn = BitConverter.ToString(encodedBytes).Replace("-", string.Empty).ToUpper(); }
-                else { _tmpReturn = BitConverter.ToString(encodedBytes); }
-
-                return _tmpReturn;
-            }
-        }
+            return _tmpReturn;
+         }
+      }
 
 
-        /// <summary>
-        /// Convert a input string to a byte array and compute the hash.
-        /// </summary>
-        /// <param name="value">Input string.</param>
-        /// <returns>The Hexadecimal string.</returns>
-        public static string ToSHA256Hash(string value, bool removeDashes = true)
-        {
-            if (value.NullOrEmpty())
-            {
-                return value.ToString(true);
-            }
-
-            using (var hasher = SHA256.Create())
-            {
-                var originalBytes = System.Text.Encoding.UTF8.GetBytes(value);
-                var encodedBytes = hasher.ComputeHash(originalBytes);
-                string _tmpReturn = "";
-                if (removeDashes) { _tmpReturn = BitConverter.ToString(encodedBytes).Replace("-", string.Empty).ToUpper(); }
-                else { _tmpReturn = BitConverter.ToString(encodedBytes); }
-
-                return _tmpReturn;
-            }
-        }
-    }
+      /// <summary>
+      /// Convert a input string to a byte array and compute the hash.
+      /// </summary>
+      /// <param name="value">Input string.</param>
+      /// <returns>The Hexadecimal string.</returns>
+      public static string ToSHA256Hash(string value, bool removeDashes = true)
+      {
+         return SHAHashing.ToSHA256Hash(value, removeDashes);
+      }
+   }
 }
 
 
